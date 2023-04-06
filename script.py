3a81d48ecc011e30ae51d1d21d6a06c2d16d5ffb
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-06 05:33:30 +0200

    step 6

--- script.py
+++ script.py
@@ -7,8 +7,16 @@ toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies"
 prices = [2, 6, 1, 3, 2, 7, 2]
 
 # Step 3
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+
 # Step 4
+num_pizzas = len(toppings)
+print(num_pizzas)
+
 # Step 5
+print("We sell "+str(num_pizzas)+" different kinds of pizza!")
+
 # Step 6
 # Step 7
 # Step 8
